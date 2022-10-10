90d75e2ea4d934ea13cf9de733e1dd997f210367
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-10-10 13:01:19 +0200

    Area exercise

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace PracticasDAP
     {
         static void Main(string[] args)
         {
-
+            // get circle area from its radius
+            Console.WriteLine("Introduzca un radio para el circulo: ");
+            // int radius = Convert.ToInt32(Console.ReadLine()); // Another way of converting Int
+            int radius = int.Parse(Console.ReadLine());
+            double area = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine($"El area del circulo de {radius} cm es {area} cm^2");
         }
     }
 }
